de8185bbbbf4ead73067ed1f5d05a60fe1509f35
Pranav Sibi <pranavsibi29@gmail.com>
2025-05-02 02:04:30 +0530

    Updated ones

--- app.py
+++ app.py
@@ -110,4 +110,4 @@ def dashboard():
     return render_template('dashboard.html', feed=intrusion_feed)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=10000)
